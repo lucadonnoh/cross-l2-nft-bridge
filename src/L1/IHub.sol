453ed568c631314ccb3650cb3f5603877c9d4653
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2024-06-23 17:52:33 +0200

    Add batching (#11)

--- src/L1/IHub.sol
+++ src/L1/IHub.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.4;
 interface IHub {
     function finalizeBridgeLock(uint256 tokenId) external;
     function isLocked(uint256) external view returns (bool);
+    function finalizeBatchBridgeLock(bytes32) external;
 }
